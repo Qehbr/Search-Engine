af299383dc7566e129ba8c4f06ff57689832b699
Yuriy Rusanov <49615282+Qehbr@users.noreply.github.com>
2021-09-17 18:02:14 +0300

    Added print crawl and save to db functions

--- crawler.py
+++ crawler.py
@@ -1,86 +1,108 @@
 #
-# Simple Web Crawler
+# Surfearch Web Crawler module
 #
 
 # Libraries
 import json
 import requests
 from bs4 import BeautifulSoup
-
-# starting url to crawl
-start_url = 'https://quotes.toscrape.com'
-# depth to go
-depth = 2
-# array of data crawled
-crawledData = []
-# tags that will be stored
-textTags = ['p', 'div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+import pymongo
 
 
-#
-# crawl function
-#
-def crawl(url, depth):
-    # perform http get request
+class Crawler():
+    # Connecting to database
+    print("Connecting to database")
     try:
-        print('Crawling URL: "%s" on depth: "%d" ' % (url, depth))
-        response = requests.get(url)
-    except:
-        print('Failed to perform HTTP GET request on "%s"\n' % url)
-        return
+        client = pymongo.MongoClient(
+            "mongodb+srv://<username>:<password>@<dbname>.awgu4.mongodb.net/<dbname>?retryWrites=true&w=majority")
+        db = client.searchResults
+        print("Sucessfully connected to database")
+    except Exception:
+        print('Error connection to database')
 
-    # get page in content variable
-    content = BeautifulSoup(response.text, 'lxml')
+    # array of data crawled
+    searchResults = []
+    # tags that will be stored
+    # TODO use tags
+    # textTags = ['p', 'div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+    textTags = ['p']
 
-    # get description from the site from allowed tags above in textTags variable
-    description = ""
-    try:
-        for tag in content.findAll():
-            if tag.name in textTags:
-                description += tag.text.strip().replace('\n', ' ')
-    except:
-        pass
+    #
+    # crawl function
+    #
 
-    # get title from the site
-    try:
-        title = content.find('title').text
-    except:
-        title = ""
-
-    # create result object containing data
-    result = {
-        'url': url,
-        'title': title.strip().replace('\n', ''),
-        'description': description.strip().replace('\n', '')
-    }
-
-    # add it to crawledData array
-    crawledData.append(result)
-
-    # if depth is exhausted
-    if depth == 0:
-        return
-
-    # get all links
-    links = content.findAll('a')
-    # crawl each link
-    for link in links:
-
+    def crawl(self, url, depth):
+        # perform http get request
         try:
-            if 'http' in link['href']:
-                crawl(link['href'], depth - 1)
-        except KeyError:
+            print('Crawling URL: "%s" on depth: "%d" ' % (url, depth))
+            response = requests.get(
+                url, headers={'user-agent': 'search-engine-crawler'})
+        except:
+            print('Failed to perform HTTP GET request on "%s"\n' % url)
+            return
+
+        # get page in content variable
+        content = BeautifulSoup(response.text, 'lxml')
+
+        # get description from the site from allowed tags above in textTags variable
+        description = ""
+        try:
+            for tag in content.findAll():
+                if tag.name in self.textTags:
+                    description += tag.text.strip().replace('\n', ' ')
+        except:
             pass
-    return
+
+        # get title from the site
+        try:
+            title = content.find('title').text
+        except:
+            title = ""
+
+        # create result object containing data
+        result = {
+            'url': url,
+            'title': title.strip().replace('\n', ''),
+            'description': description.strip().replace('\n', '')
+        }
+
+        # add it to searchResults array
+        self.searchResults.append(result)
+
+        # if depth is exhausted
+        if depth == 0:
+            return
+
+        # get all links
+        links = content.findAll('a')
+        # crawl each link
+        for link in links:
+
+            try:
+                if 'http' in link['href']:
+                    self.crawl(link['href'], depth - 1)
+            except KeyError:
+                pass
+    
+    # Print array of searchResults 
+    def printData(self):
+        # for entry in self.searchResults:
+        #     print(entry)
+        # print("\nSuccess! Nubmer of entries scraped", len(self.searchResults))
+
+        for entry in self.db.searchResults.find({'$text':{'$search':'Quotes to Scrape'}}):
+            print(entry)
+
+    # Insets results to database 
+    def insertResults(self):
+        searchResults = self.db.searchResults
+        searchResults.insert_many(self.searchResults)
+        searchResults.create_index([
+            ('url', pymongo.TEXT),
+            ('title', pymongo.TEXT),
+            ('description', pymongo.TEXT),
+
+        ], name='searchResults')
 
 
-# calling crawl function with given depth
-crawl(start_url, depth)
 
-# TODO tempoorarily writing crawled data to JSON file
-with open('data.json', 'w') as json_file:
-    fileContent = ''
-    for entry in crawledData:
-        fileContent += json.dumps(entry, indent=2)
-    json_file.write(fileContent)
-    print("Success! Length of crawled data", len(crawledData))
